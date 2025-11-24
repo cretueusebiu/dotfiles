b3ec90520ece94a083461b86f6eeee0d8aa01215
Cretu Eusebiu <1517945+cretueusebiu@users.noreply.github.com>
2025-11-24 08:47:32 +0200

    Update

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ terminalDir=~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe
 
 if [ -d "$terminalDir" ]; then
     cp ./windows-terminal-settings.json "$terminalDir/LocalState/settings.json"
-    cp ./git-bash.ico "$terminalDir/RoamingState"
-    cp ./ubuntu.ico "$terminalDir/RoamingState"
+    cp ./icons/. "$terminalDir/RoamingState"
 fi
 
 echo "Installing npm dependencies..."
